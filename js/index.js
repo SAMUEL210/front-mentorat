feade04adf9c8346a196062225c31ceb09d397aa
Samuel <samuel210@live.fr>
2021-08-28 07:19:30 +0200

    Fin configuration Connexion et Creer compte Correction et ajout de la page d'acceuil

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const validerConnexion = document.querySelector('.se-connecter')
-const validerCreerCompte = document.querySelector('.creer-compte')
+const validerCreerCompte = document.getElementById('creer-compte')
 
 // Conenxion
 const email = document.getElementById('email')
@@ -13,11 +13,13 @@ const nom = document.getElementById('nom')
 const prenom = document.getElementById('prenom')
 const crEmail = document.getElementById('cr-email')
 const crMp = document.getElementById('cr-mp')
-const confMp = document.getElementById('conf-fr-mp')
+const confMp = document.getElementById('conf-cr-mp')
 const poste = document.getElementById('poste')
 const entreprise = document.getElementById('entreprises')
 const crChampsVides = document.querySelector('.cr-champs-vides')
-const donneesIncorrectes = document.querySelector('.donnees-incorrectes')
+const emailIncorrectes = document.querySelector('.email-incorrectes')
+const succes = document.querySelector('.cr-succes')
+const mpDifferent = document.querySelector('.mp-different')
 var checkbox2 = document.getElementById("cr-afficher-mp");
 
 
@@ -59,7 +61,8 @@ validerConnexion.addEventListener('click', (e) => {
                 } else {
                     identifiantsIncorrectes.classList.remove('affiche')
                     identifiantsIncorrectes.classList.add('masque')
-                        //load page d'accueil
+
+                    window.location = './accueil/index.html'
                 }
             });
     }
@@ -69,45 +72,132 @@ validerConnexion.addEventListener('click', (e) => {
 validerCreerCompte.addEventListener('click', (e) => {
     e.preventDefault();
     console.log('Clcik creer-compte')
+
     if (nom.value == "" || prenom.value == "" || crEmail.value == "" || crMp.value === "" || confMp.value == "" || poste.value == "" || entreprises == "") {
-        champsvides.classList.remove('masque')
-        champsvides.classList.add('affiche')
+        crChampsVides.classList.remove('masque')
+        crChampsVides.classList.add('affiche')
 
-        if (email.value == "")
-            email.classList.add('is-invalid')
-        else email.classList.remove('is-invalid')
-        if (mp.value == "")
-            mp.classList.add('is-invalid')
-        else mp.classList.remove('is-invalid')
-    } else if (email.value != "" && mp.value != "") {
-        champsvides.classList.remove('affiche')
-        champsvides.classList.add('masque')
-        mp.classList.remove('is-invalid')
-        email.classList.remove('is-invalid')
+        if (nom.value == "")
+            nom.classList.add('is-invalid')
+        else {
+            nom.classList.remove('is-invalid')
+            nom.classList.add('is-valid')
+        }
 
-        // axios({
-        //         method: 'post',
-        //         url: 'http://localhost:3000/login',
-        //         data: {
-        //             email: email.value,
-        //             mp: mp.value
-        //         },
-        //         responseType: 'json'
-        //     })
-        //     .then(function(reponse) {
-        //         if (reponse.data.error) {
-        //             identifiantsIncorrectes.innerHTML = reponse.data.error
-        //             identifiantsIncorrectes.classList.remove('masque')
-        //             identifiantsIncorrectes.classList.add('affiche')
+        if (prenom.value == "")
+            prenom.classList.add('is-invalid')
+        else {
+            prenom.classList.remove('is-invalid')
+            prenom.classList.add('is-valid')
+        }
 
-        //             if (reponse.data.cause == 'Email')
-        //                 email.classList.add('is-invalid')
-        //         } else {
-        //             identifiantsIncorrectes.classList.remove('affiche')
-        //             identifiantsIncorrectes.classList.add('masque')
-        //                 //load page d'accueil
-        //         }
-        //     });
+        if (crEmail.value == "")
+            crEmail.classList.add('is-invalid')
+        else {
+            crEmail.classList.remove('is-invalid')
+            crEmail.classList.add('is-valid')
+        }
+
+        if (crMp.value == "")
+            crMp.classList.add('is-invalid')
+        else {
+            crMp.classList.remove('is-invalid')
+            crMp.classList.add('is-valid')
+        }
+
+        if (confMp.value == "")
+            confMp.classList.add('is-invalid')
+        else {
+            confMp.classList.remove('is-invalid')
+            confMp.classList.add('is-valid')
+        }
+
+        if (poste.value == "")
+            poste.classList.add('is-invalid')
+        else {
+            poste.classList.remove('is-invalid')
+            poste.classList.add('is-valid')
+        }
+
+        if (entreprises.value == "")
+            entreprises.classList.add('is-invalid')
+        else {
+            entreprises.classList.remove('is-invalid')
+            entreprises.classList.add('is-valid')
+        }
+
+    } else if (nom.value != "" && prenom.value != "" && crEmail.value != "" && crMp.value != "" && confMp.value != "" && poste.value != "" && entreprises.value != "") {
+        crChampsVides.classList.remove('affiche')
+        crChampsVides.classList.add('masque')
+
+        nom.classList.remove('is-invalid')
+        prenom.classList.remove('is-invalid')
+        poste.classList.remove('is-invalid')
+        entreprises.classList.remove('is-invalid')
+
+        nom.classList.add('is-valid')
+        prenom.classList.add('is-valid')
+        poste.classList.add('is-valid')
+        entreprises.classList.add('is-valid')
+
+        if (crMp.value != confMp.value) {
+            crMp.classList.add('is-invalid')
+            confMp.classList.add('is-invalid')
+            mpDifferent.classList.remove('masque')
+            mpDifferent.classList.add('affiche')
+        } else {
+            crMp.classList.remove('is-invalid')
+            crMp.classList.add('is-valid')
+            confMp.classList.remove('is-invalid')
+            confMp.classList.add('is-valid')
+            mpDifferent.classList.remove('affiche')
+            mpDifferent.classList.add('masque')
+
+            axios({
+                    method: 'post',
+                    url: 'http://localhost:3000/utilisateurs',
+                    data: {
+                        nom: nom.value,
+                        prenom: prenom.value,
+                        email: crEmail.value,
+                        mp: crMp.value,
+                        poste: poste.value,
+                        entreprise_id: entreprises.value
+                    },
+                    responseType: 'json'
+                })
+                .then(function(reponse) {
+                    if (reponse.data.error) {
+                        emailIncorrectes.innerHTML = reponse.data.error
+                        emailIncorrectes.classList.remove('masque')
+                        emailIncorrectes.classList.add('affiche')
+
+                        if (reponse.data.cause == 'Email')
+                            crEmail.classList.add('is-invalid')
+                    } else {
+                        nom.classList.remove('is-valid')
+                        prenom.classList.remove('is-valid')
+                        crEmail.classList.remove('is-invalid')
+                        crMp.classList.remove('is-valid')
+                        confMp.classList.remove('is-valid')
+                        poste.classList.remove('is-valid')
+                        entreprises.classList.remove('is-valid')
+
+                        emailIncorrectes.classList.remove('affiche')
+                        emailIncorrectes.classList.add('masque')
+
+                        nom.value = ''
+                        prenom.value = ''
+                        crEmail.value = ''
+                        crMp.value = ''
+                        confMp.value = ''
+                        poste.value = ''
+
+                        succes.classList.remove('masque')
+                        succes.classList.add('affiche')
+                    }
+                });
+        }
     }
 
 })
