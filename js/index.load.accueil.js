c262010eff3d44c07c698b8b5e927e198e0e1646
Samuel <samuel210@live.fr>
2021-10-06 16:40:30 +0200

    Affichage des contrats dans la page d'accueil

--- js/index.load.accueil.js
+++ js/index.load.accueil.js
@@ -2,8 +2,6 @@ if (!(sessionStorage.getItem('utilisateur') && sessionStorage.getItem('x-access-
     window.location.replace('../index.html')
 } else {
 
-
-
     const titre = document.querySelector('.titre')
     const collegues = document.querySelector('.collegues')
     const contrats = document.querySelector('.contrats')
@@ -78,8 +76,50 @@ if (!(sessionStorage.getItem('utilisateur') && sessionStorage.getItem('x-access-
         })
         .then(function(reponse) {
             for (contrat of reponse.data.contrats) {
-                if (contrat.mentorId != sessionStorage.getItem('utilisateur')) {}
-            }
-        })
+                if (contrat.mentorId == sessionStorage.getItem('utilisateur')) {
+                    var utId = contrat.mentoreId
+                }
+                if (contrat.mentoreId == sessionStorage.getItem('utilisateur')) {
+                    var utId = contrat.mentorId
+                }
+                if (contrat.mentorId == sessionStorage.getItem('utilisateur') || contrat.mentoreId == sessionStorage.getItem('utilisateur')) {
+                    axios({
+                        method: 'get',
+                        url: 'https://api-simarone-fusecrum.herokuapp.com/utilisateurs/' + utId,
+                        headers: {
+                            'x-access-token': sessionStorage.getItem('x-access-token')
+                        },
+                        responseType: 'json'
+                    }).then(function(donneeUtId) {
+                        var ut = donneeUtId.data.utilisateur
+                        let div = document.createElement('div')
+                        let p = document.createElement('p')
+                        let form = document.createElement('form')
+                        let input = document.createElement('input')
+                        let button = document.createElement('input')
 
+                        p.innerHTML = "Avec " + ut.prenom + ' ' + ut.nom + ', ' + ut.poste
+                        p.className = 'p-contrats'
+                        div.appendChild(p)
+
+                        input.style.display = 'none'
+                        input.type = 'text'
+                        input.value = ut.entreprise_id
+                        form.appendChild(input)
+
+                        button.value = 'Voir plus'
+                        button.type = 'submit'
+                        button.className = 'btn btn-outline-danger btn-sm'
+                        form.appendChild(button)
+                        div.appendChild(form)
+                        contrats.appendChild(div)
+
+                        div.style.display = 'flex'
+                        div.style.justifyContent = 'space-between'
+                        div.className = 'div-contrats breadcrumb'
+                    });
+
+                }
+            }
+        });
 }
